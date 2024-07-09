e3fdbc8aaeadcb35ce361646ffc598ddc0bc49f6
Durpa <durpayt@gmail.com>
2024-07-09 02:15:11 -0600

    welcome

---
+++ src/main/java/net/durpa/welcome/WelcomeClient.java
@@ -0,0 +1,10 @@
+package net.durpa.welcome;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WelcomeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/durpa/welcome/WelcomeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.durpa.welcome;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WelcomeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
